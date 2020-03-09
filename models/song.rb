c8b63017ab2c6d1052970e1e5503be86ff9d2c98
Miguel Elizondo <miguelaea015@gmail.com>
2020-03-08 19:22:54 -0700

    feat: create the Song model

---
+++ models/song.rb
@@ -0,0 +1,25 @@
+require "mongoid"
+
+class Song
+
+  include Mongoid::Document
+
+  field :code, type: Integer
+  field :title, type: String
+  field :artist, type: String
+  field :release_year, type: Integer
+  field :genre, type: String
+  field :duration, type: Float
+  field :album, type: String
+  field :total_views, type: Integer
+
+  validates :code, presence: true
+  validates :title, presence: true
+  validates :artist, presence: true
+  validates :release_year, presence: true
+  validates :genre, presence: true
+  validates :duration, presence: true
+  validates :album, presence: true
+  validates :total_views, presence: true
+
+end
