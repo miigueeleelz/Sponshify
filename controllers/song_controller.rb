64db62f540878fee4e176734fbd377da0207211e
Miguel Elizondo <miguelaea015@gmail.com>
2020-03-08 19:26:42 -0700

    feat: add method store used to create new songs

--- controllers/song_controller.rb
+++ controllers/song_controller.rb
@@ -1,7 +1,33 @@
 require "singleton"
+require_relative "../models/song"
 
 class SongController
 
   include Singleton
+
+  def store(song)
+    result = Song.create(
+      code: song["code"],
+      title: song["title"],
+      artist: song["artist"],
+      release_year: song["release_year"],
+      genre: song["genre"],
+      duration: song["duration"],
+      album: song["album"],
+      total_views: 0,
+    )
+
+    if result.valid?
+      response = {
+        message: 'Song created successfully'
+      }
+      return [201, {}, response.to_json]
+    end
+
+    response = {
+      errors: result.errors.full_messages
+    }
+    return [422, {}, response.to_json]
+  end
 
 end
